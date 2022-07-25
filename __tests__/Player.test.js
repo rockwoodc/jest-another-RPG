b252f278ca4e808728e24f47e225d6455f0bcae1
rockwoodc <rockwoodce@gmail.com>
2022-07-25 10:51:39 -0400

    get & use  new potions object added

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,6 +4,7 @@ jest.mock('../lib/Potion');
 console.log(new Potion());
 
 const Player = require('../lib/Player');
+// const { default: test } = require('node:test');
 
 test ('creates a player object', ()=> {
     const player = new Player('Dave');
@@ -60,3 +61,29 @@ test('gets inventory from player or returns false', () => {
   
     expect(player.health).toBe(0);
   });
+
+  test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+  });
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
