4dc9b7ddac999227926920a7ccba1afde0828450
rockwoodc <rockwoodce@gmail.com>
2022-07-25 10:22:55 -0400

    added tests and objects for getting and reducing health

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -36,3 +36,27 @@ test('gets inventory from player or returns false', () => {
   
     expect(player.getInventory()).toEqual(false);
   });
+
+  //test to recieve health info
+  test('gets players health value', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+
+  });
+
+  //tests if the correct amount of health is being subtracted
+  test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
+  });
